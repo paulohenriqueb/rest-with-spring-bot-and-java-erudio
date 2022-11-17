8a5b1620d561e0cbc97a88617fe0c9341d89e8f2
Paulo Henrique B da Silva <paulohenriqueb.silva@gmail.com>
2022-11-17 11:30:26 -0300

    Criado o methodo de listagem de todas as pessoas

--- 06_parametersAndExceptionHandler/rest/src/main/java/paulohenrique/rest/controllers/PersonController.java
+++ 06_parametersAndExceptionHandler/rest/src/main/java/paulohenrique/rest/controllers/PersonController.java
@@ -1,5 +1,7 @@
 package paulohenrique.rest.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,10 +26,14 @@ public class PersonController {
 	@Autowired
 	private PersonServices personService; 
 	
+	@RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Person> findAll(){
+		return personService.findAll();
+	}
+	
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person findById( @PathVariable(value = "id") String id) throws Exception{
+	public Person findById( @PathVariable(value = "id") String id){
 		return personService.findById(id);
-		
 	}
 	
 }
--- 06_parametersAndExceptionHandler/rest/src/main/java/paulohenrique/rest/services/PersonServices.java
+++ 06_parametersAndExceptionHandler/rest/src/main/java/paulohenrique/rest/services/PersonServices.java
@@ -1,5 +1,7 @@
 package paulohenrique.rest.services;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -13,6 +15,18 @@ public class PersonServices {
 	
 	private Logger logger = Logger.getLogger(PersonServices.class.getName());
 	
+	public List<Person> findAll() {
+		logger.info("Finding All people");
+		List<Person> persons = new ArrayList<>();
+		
+		for (int i =0; i < 8; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+		}
+		
+		return persons;
+	}
+	
 	public Person findById(String id) {
 		logger.info("Finding one person!");
 		Person person = new Person();
@@ -21,6 +35,15 @@ public class PersonServices {
 		person.setLastName("Silva");
 		person.setAddress("Carapicuíba - São Paulo - Brasil");
 		person.setGender("Male");
+		return person;
+	}
+	private Person mockPerson(int i) {
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Person name " + i);
+		person.setLastName("Last name " + i);
+		person.setAddress("Addres " + i);
+		person.setGender("Gender " + i);
 		return person;
 	}
 }
