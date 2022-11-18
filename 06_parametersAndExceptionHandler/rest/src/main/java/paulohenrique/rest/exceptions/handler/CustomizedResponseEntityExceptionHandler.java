1ebac7617d6ecc50514bdf0c5cf03af967907a40
Paulo Henrique B da Silva <paulohenriqueb.silva@gmail.com>
2022-11-18 12:46:37 -0300

    Instanciação com o banco retirada dos dados mock, refatoração do controler

--- 06_parametersAndExceptionHandler/rest/src/main/java/paulohenrique/rest/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
+++ 06_parametersAndExceptionHandler/rest/src/main/java/paulohenrique/rest/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
@@ -11,7 +11,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import paulohenrique.rest.exceptions.ExceptionResponse;
-import paulohenrique.rest.exceptions.UnsupportedMathOperationException;
+import paulohenrique.rest.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
 @RestController
@@ -28,14 +28,14 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 		return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	
-	@ExceptionHandler(UnsupportedMathOperationException.class)
+	@ExceptionHandler(ResourceNotFoundException.class)
 	public final ResponseEntity<ExceptionResponse> 
-	handleBadRequestExceptions(Exception ex, WebRequest request){
+	handleNotFoundExceptions(Exception ex, WebRequest request){
 		ExceptionResponse exceptionResponse = new ExceptionResponse(
 				new Date(),
 				ex.getMessage(),
 				request.getDescription(false)
 				);
-		return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
 	}
 }
--- 06_parametersAndExceptionHandler/rest/src/main/java/paulohenrique/rest/services/PersonServices.java
+++ 06_parametersAndExceptionHandler/rest/src/main/java/paulohenrique/rest/services/PersonServices.java
@@ -1,66 +1,52 @@
 package paulohenrique.rest.services;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
-import java.util.logging.Logger;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import paulohenrique.rest.exceptions.ResourceNotFoundException;
 import paulohenrique.rest.model.Person;
+import paulohenrique.rest.repositories.PersonRepository;
 
 @Service
 public class PersonServices {
-	private final AtomicLong counter = new AtomicLong();
 	
-	private Logger logger = Logger.getLogger(PersonServices.class.getName());
+	@Autowired
+	PersonRepository personRepository;
 	
 	public List<Person> findAll() {
-		logger.info("Finding All people");
-		List<Person> persons = new ArrayList<>();
-		
-		for (int i =0; i < 8; i++) {
-			Person person = mockPerson(i);
-			persons.add(person);
-		}
-		
-		return persons;
+		return personRepository.findAll();
 	}
 	
-	public Person findById(String id) {
-		logger.info("Finding one person!");
-		Person person = new Person();
-		person.setId(counter.incrementAndGet());
-		person.setFirstName("Paulo");
-		person.setLastName("Silva");
-		person.setAddress("Carapicuíba - São Paulo - Brasil");
-		person.setGender("Male");
-		return person;
+	public Person findById(Long id) {
+		
+		return personRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("No records found for this id!"));
 	}
 	
 	public Person create(Person person) {
-		logger.info("Creating one person!");
 		
-		return person;
-	}
-	
-	public Person update( Person person) {
-		logger.info("Updating one person");
 		
-		return person;
+		return personRepository.save(person);
 	}
 	
-	public void delete(long id) {
-		logger.info("Deleting one person!");
+	public Person update(Person person) {
+		Person entity = personRepository.findById(person.getId())
+			.orElseThrow(()-> new ResourceNotFoundException("Error"));
+		
+		entity.setFirstName(person.getFirstName());
+		entity.setLastName(person.getLastName());
+		entity.setAddress(person.getAddress());
+		entity.setGender(person.getGender());
+		
+		return personRepository.save(person);
 	}
 	
-	private Person mockPerson(int i) {
-		Person person = new Person();
-		person.setId(counter.incrementAndGet());
-		person.setFirstName("Person name " + i);
-		person.setLastName("Last name " + i);
-		person.setAddress("Addres " + i);
-		person.setGender("Gender " + i);
-		return person;
+	public void delete(Long id) {
+		Person entity = personRepository.findById(id)
+				.orElseThrow(()-> new ResourceNotFoundException("Error to delete"));
+		personRepository.delete(entity);
 	}
+	
 }
