2bab22365fc730c969c2571ad12869f9fffb46b9
Paulo Henrique B da Silva <paulohenriqueb.silva@gmail.com>
2022-11-21 13:54:13 -0300

    Content negotiation - implementado a forma de representar os dados de outra forma além do JSON(foi implementado a representação de dados através do XML)

--- 06_parametersAndExceptionHandler/rest/src/main/java/paulohenrique/rest/controllers/PersonController.java
+++ 06_parametersAndExceptionHandler/rest/src/main/java/paulohenrique/rest/controllers/PersonController.java
@@ -31,22 +31,46 @@ public class PersonController {
 	@Autowired
 	private PersonServices personService; 
 	
-	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(
+		produces = { 
+			MediaType.APPLICATION_JSON_VALUE, 
+			MediaType.APPLICATION_XML_VALUE 
+		})
 	public List<PersonVO> findAll(){
 		return personService.findAll();
 	}
 	
-	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(value = "/{id}", 
+		produces = { 
+			MediaType.APPLICATION_JSON_VALUE,
+			MediaType.APPLICATION_XML_VALUE 
+		})
 	public PersonVO findById( @PathVariable(value = "id") Long id){
 		return personService.findById(id);
 	}
 	
-	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@PostMapping(
+		consumes = { 
+			MediaType.APPLICATION_JSON_VALUE, 
+			MediaType.APPLICATION_XML_VALUE 
+		}, 
+		produces = { 
+			MediaType.APPLICATION_JSON_VALUE,
+			MediaType.APPLICATION_XML_VALUE 
+		})
 	public PersonVO create(@RequestBody PersonVO person) {
 		return personService.create(person);
 	}
 	
-	@PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@PutMapping(value = "/{id}", 
+		consumes = { 
+			MediaType.APPLICATION_JSON_VALUE, 
+			MediaType.APPLICATION_XML_VALUE 
+		}, 
+		produces = { 
+			MediaType.APPLICATION_JSON_VALUE, 
+			MediaType.APPLICATION_XML_VALUE 
+		})
 	public PersonVO update( @RequestBody PersonVO person) {
 		
 		return personService.update(person);
