d33222a80c273b157e0b4471aa7a86e9b2eebda8
Paulo Henrique B da Silva <paulohenriqueb.silva@gmail.com>
2022-12-04 19:23:14 -0300

    Criado as classes que irá intermediar o token e excessão de authenticação

--- 06_parametersAndExceptionHandler/rest/src/main/java/paulohenrique/rest/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
+++ 06_parametersAndExceptionHandler/rest/src/main/java/paulohenrique/rest/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
@@ -2,6 +2,7 @@ package paulohenrique.rest.exceptions.handler;
 
 import java.util.Date;
 
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -13,6 +14,7 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 import paulohenrique.rest.exceptions.ExceptionResponse;
 import paulohenrique.rest.exceptions.RequiredObjectIsNullException;
 import paulohenrique.rest.exceptions.ResourceNotFoundException;
+import paulohenrique.rest.exceptions.InvalidJwtAuthenticationException;
 
 @ControllerAdvice
 @RestController
@@ -48,5 +50,15 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 				request.getDescription(false)
 				);
 		return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(InvalidJwtAuthenticationException.class)
+	public final ResponseEntity<ExceptionResponse> 
+	InvalidJwtAuthenticationException(Exception ex, WebRequest request){
+		ExceptionResponse exceptionResponse = new ExceptionResponse(
+				new Date(),
+				ex.getMessage(),
+				request.getDescription(false));
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.FORBIDDEN);
 	}
 }
